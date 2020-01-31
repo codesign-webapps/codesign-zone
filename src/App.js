f4a97728be600576a9766bb3bcbef2ed521574d0
Andrew Brooking <atbrooking.dev@gmail.com>
2020-01-31 14:13:54 -0600

    Started adding parallax

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Parallax pages={3}>
+      
+      <ParallaxLayer offset={1} speed={1} factor={1} className="bg-yellow-light" />
+      <ParallaxLayer offset={2} speed={1} factor={1} className="bg-yellow-dark" />
+
+      <ParallaxLayer offset={0} speed={0.8} style={{ opacity: 0.2 }}>
+        <img src="./images/rect-dark-gray-200x400.png" alt="" style={{ display: 'block', width: '10%', marginLeft: '55%' }} />
+        <img src="./images/square-gray-200.png" alt="" style={{ display: 'block', width: '15%', marginLeft: '15%' }} />
+        <img src="./images/rect-light-gray-200x800.png" alt="" style={{ display: "block", width: "10%", marginLeft: "40%" }} />
+      </ParallaxLayer>
+
+      <ParallaxLayer offset={0} speed={-0.1}>
+        <img src="./images/logo.png" alt="" style={{ display: "block", width: "60%", margin: "2rem auto" }} />
+      </ParallaxLayer>
+
+    </Parallax>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
 import { Provider } from 'react-redux'
-import store from './store'
+import store from './redux/store';
 
 const rootElement = document.getElementById("root");
 
