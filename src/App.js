835f72fb2714a3132b09ebba78c828c72226908c
Andrew Brooking <atbrooking.dev@gmail.com>
2020-02-04 14:25:35 -0600

    Finished Landing section content and added content for about section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons'
+import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons';
+import { Container, Button, Typography } from '@material-ui/core';
 import Shape from './components/Shape';
 import './App.css';
 
@@ -12,6 +13,7 @@ class App extends React.Component {
          {/* Site Content */}
         <Parallax ref={ref => (this.parallax = ref)} pages={3}>
           
+          {/* Dark Backgrounds */}
           <ParallaxLayer offset={1} speed={1} factor={1} className="bg-dark" />
           <ParallaxLayer offset={2} speed={1} factor={1} className="bg-dark" />
 
@@ -28,17 +30,56 @@ class App extends React.Component {
           {/* Landing Page Section */}
           <ParallaxLayer offset={0} speed={-0.13}>
             {/* Logo */}
-            <img src="./images/logo.png" alt="" style={{ display: "block", width: "47%", margin: "1.5rem auto" }} />
+            <img src="./images/logo.png" alt="" style={{ display: "block", width: "50%", margin: "1.5rem auto" }} />
 
-            {/* Get Quote Button */}
+            <Container maxWidth="sm" >
+              {/* Get Quote Button */}
+              <Button variant="outlined" size="large" style={{ float: "left" }}>
+                <Typography variant="h4">Get Quote</Typography>
+              </Button>
 
-            {/* Portfolio Button */}
+              {/* Portfolio Button */}
+              <Button variant="outlined" size="large" style={{ float: "right" }}>
+              <Typography variant="h4">Portfolio</Typography>
+              </Button>
+            </Container>
+          </ParallaxLayer>
 
+          <ParallaxLayer offset={0.9} speed={0.5}>
             {/* Scroll Icon */}
-
+            <Container maxWidth="xs" style={{ margin: "0 auto"}}>
+              <span class="scroll-icon" style={{margin: "0 auto"}}>
+                <span class="scroll-icon__dot"></span>
+              </span>
+            </Container>
           </ParallaxLayer>
 
           {/* About Section */}
+          <ParallaxLayer offset={1.1} speed={1} className="center" className="text-yellow">
+            {/* Section Title */}
+            <Typography variant="h2" component="h2">Who are we anyway?</Typography>
+
+            {/* About Text */}
+            <Typography variant="h4" component="h2">Codesign Web Applications</Typography>
+            <Typography variant="body1" component="p">
+              is an experienced and passionate group of designers, developers, project managers, writers, and artists.
+              Built on customer service first and foremost, every customer becomes a part of the Codesign family. Together we face the challenges and celebrate the victories!
+            </Typography>
+
+            {/* What We Do Text */}
+            <Typography variant="h4" component="h2">What We Do</Typography>
+            <ul>
+              <li>Web Design</li>
+              <li>Web Hosting</li>
+              <li>App Development</li>
+              <li>Web Copy Writing</li>
+              <li>Photography</li>
+              <li>Videography</li>
+            </ul>
+
+            {/* Shapes */}
+
+          </ParallaxLayer>
 
           {/* Process Section */}
 
