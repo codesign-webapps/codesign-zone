0618c5e2789c5a6e132e939d4bb12954dae8c20c
Ryan Harris <ragobash@gmail.com>
2020-02-22 11:24:38 -0600

    adding Process Page info and Team Page info.

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import { Typography, Grid } from '@material-ui/core';
 
 function Team() {
     return (
@@ -15,13 +15,32 @@ function Team() {
             </Typography>
 
             {/* Section Sub-Title */}
-
+            <Typography variant="h4">
+                We are the dynamic duo behind <b>codesign web apps</b>.
+            </Typography>
             {/* Section Text */}
+            <div style={{maxWidth: "50%", margin: "0 auto"}}>
+            <Typography variant="body1" style={{fontSize:"1.25rem"}}>
+            We met in a coding bootcamp through the University of Texas at Austin and quickly became friends. After finding out that we had complimentary skills in front and back end development, we decided to put them together to form the greatest web development team the world has ever known!
+            </Typography>
+            </div>
 
             {/* Andrew Picture */}
-
+            <Grid container spacing={0} style={{marginTop: "1rem"}}>
+            <Grid item xs={2}></Grid>
+            <Grid item xs={4} className="center">
+                <img src="./images/smiley-yellow.png" alt="Andrew" style={{width: "50%"}}></img>
+                <Typography variant="h5">Andrew Brooking</Typography>
+                <Typography variant="body1">co-founder</Typography>
+            </Grid>
             {/* Ryan Picture */}
-            
+            <Grid item xs={4} className="center">
+                <img src="./images/smiley-yellow.png" alt="Ryan" style={{width: "50%"}}></img>
+                <Typography variant="h5">Ryan Harris</Typography>
+                <Typography variant="body1" style={{paddingTop: "0"}}>co-founder</Typography>
+            </Grid>
+            <Grid item xs={2}></Grid>
+            </Grid>
         </div>
     );
 }
